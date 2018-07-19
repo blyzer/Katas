df25c38897b95ee52ef3fa9458c24df1341c4789
enver daniel francisco baez <enver.francisco@gmail.com>
2018-07-19 17:31:21 -0400

    arabic numerals to Roman numerals

---
+++ Katas.ArabicToRoman/ArabicNumeralsToRomanNumerals.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Katas.ArabicToRoman
+{
+    public static class ArabicNumeralsToRomanNumerals
+    {
+        /// <summary>
+        /// This Dictionary help to identify the roman number to be converted.
+        /// </summary>
+        static readonly Dictionary<string, int> romanToArabics = new Dictionary<string, int>()
+        {
+            { "ↈ" , 100000 },
+            { "ↇ" , 50000  },
+            { "ↂ", 10000  },
+            { "ↁ" , 5000   },
+            { "Z" , 2000   },
+            { "M" , 1000   },
+            { "CM", 900    },
+            { "D" , 500    },
+            { "CD", 400    },
+            { "C" , 100    },
+            { "XC", 90     },
+            { "L" , 50     },
+            { "XL", 40     },
+            { "X" , 10     },
+            { "IX", 9      },
+            { "V" , 5      },
+            { "IV", 4      },
+            { "I" , 1      }
+        };
+
+        public static string Of(int arabic)
+        {
+            if (arabic <= 0)
+            {
+                return "is emtpy the number";
+            }
+
+
+            string result = "";
+
+            foreach (var romanToArabic in romanToArabics)
+            {
+                while (arabic >= romanToArabic.Value)
+                {
+                    result += romanToArabic.Key;
+                    arabic -= romanToArabic.Value;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Katas/Program.cs
+++ Katas/Program.cs
@@ -6,25 +6,13 @@ using System.Text.RegularExpressions;
 using System.Data;
 using System.Collections;
 using System.Linq.Expressions;
+using Katas.ArabicToRoman;
 
 namespace Katas.Wrap
 {
     public static class Program
     {
-        /// <summary>
-        /// This Dictionary help to identify the roman number to be converted.
-        /// </summary>
-        static Dictionary<string, int> RomanMap = new Dictionary<string, int>()
-        {
-            {" ", 0 },
-            {"I", 1},
-            {"V", 5},
-            {"X", 10},
-            {"L", 50},
-            {"C", 100},
-            {"D", 500},
-            {"M", 1000}
-        };        
+
 
         public static void Main()
         {
@@ -32,7 +20,7 @@ namespace Katas.Wrap
             int ColumText = 0;
             string textParam = "";
 
-            while (choosenProgram == 1 || choosenProgram == 2 || choosenProgram == 3)
+            while (choosenProgram != 0)
             {
 
                 Console.WriteLine("Choose an option: \n 1: Word Wrap \n 2: Arabic number to Roman number Converter \n 3: String Calculator \n 0: to exit");
@@ -51,16 +39,9 @@ namespace Katas.Wrap
                 }
                 else if (choosenProgram == 2)
                 {
-                    Console.WriteLine(RomanNumnber());
-                }
-                else if (choosenProgram == 3)
-                {
-
-                    Console.WriteLine("Enter Numbers");
-                    textParam = Console.ReadLine();
-                    //tesp();
-                    Console.WriteLine(StringCalculator(textParam));
-                    Console.WriteLine(WordWrap(textParam));
+                    Console.WriteLine("enter");
+                    int.TryParse(Console.ReadLine(), out ColumText);
+                    Console.WriteLine(ArabicNumeralsToRomanNumerals.Of(ColumText));
                 }
                 else if (choosenProgram == 0)
                 {
@@ -72,40 +53,6 @@ namespace Katas.Wrap
                 }
             }
 
-        }
-
-        public static string RomanNumnber()
-        {
-            string stringRomanNumber;
-            int numberQuantity;
-
-            Console.WriteLine("convert any quantity of Arabic number to convert");
-            stringRomanNumber = Console.ReadLine();
-
-            numberQuantity = RomanToInteger(stringRomanNumber);
-            return "the result is: " + numberQuantity + "\n";
-        }
-
-        public static int RomanToInteger(string roman)
-        {
-            int presentNumber = 0;
-            char previousCharToCompare = roman[0];
-            string previousCharToCompareDictionary = previousCharToCompare.ToString().ToUpper();
-            string presentChar;
-
-            foreach (char currentChar in roman)
-            {
-                presentChar = currentChar.ToString().ToUpper();
-                previousCharToCompareDictionary = previousCharToCompare.ToString().ToUpper();
-
-                presentNumber += RomanMap[presentChar];
-                if (RomanMap[previousCharToCompareDictionary] < RomanMap[presentChar])
-                {
-                    presentNumber -= RomanMap[previousCharToCompareDictionary] * 2;
-                }
-                previousCharToCompare = currentChar;
-            }
-            return presentNumber;
         }
     }
 }
