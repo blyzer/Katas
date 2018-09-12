268b06165521e0099866d17ca5ada24a70733ddc
Enver Daniel Francisco Baez <enver.francisco@gmail.com>
2018-09-11 21:55:07 -0400

    string

--- Katas.Fibonacci/test.js
+++ Katas.Fibonacci/test.js
@@ -1,13 +1,13 @@
 // Require the built in 'assertion' library
-var assert = require('assert');
-var FibonacciTest = require('../Katas.Fibonacci/fibonacci');
+var assert = require("assert");
+var FibonacciTest = require("../Katas.Fibonacci/fibonacci");
 
 // Create a group of tests about Fibonacci
-describe('fibonacci', function() {
+describe("fibonacc", function() {
     // A string explanation of what we're testing
-    it('should return 21 when the value is 8', function(){
+    it("should return 21 when the value is 8", function(){
         // Our actual test: should equal to 21
-        result = fibonacci(8);
+     var result = fibonacci(8);
         assert.equal(result, 21);
     });
 });
